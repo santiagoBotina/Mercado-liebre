6f45fd9320b72a2918407deb6fb258e4d36c8d01
santiagoBotina <santiagoboe04@gmail.com>
2021-11-11 21:05:26 -0500

    adding Procfile and editing the express routes

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.post("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor corriendo en puerto 3000");
 });
